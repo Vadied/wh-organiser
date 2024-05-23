2cc928c6f901daac6640fc31514216379b848e38
Davide Filippi <d91.filippi@gmail.com>
2024-05-23 17:00:09 +0200

    new secondary

--- app/missions/secondary/page.tsx
+++ app/missions/secondary/page.tsx
@@ -2,13 +2,13 @@ import { secondaryMissions } from "@/assets/mocks/missions";
 import Accordion from "@/components/Accordion";
 
 const Page = () => {
-  const items = secondaryMissions.map((m) => ({
-    title: m.name,
-    content: m.rule,
+  const items = secondaryMissions.map(({ name, isFixed, rule }) => ({
+    title: `${name}${isFixed ? " - fixed" : ""}`,
+    content: rule,
   }));
   return (
     <>
-      <h1 className="text-3xl mb-4">Secondary Missions</h1>
+      <h1 className="text-3xl mb-4">Missioni secondarie</h1>
       <Accordion items={items} />
     </>
   );
--- components/AccordionItem.tsx
+++ components/AccordionItem.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 const minusIcon = "-";
 const plusIcon = "+";
 
